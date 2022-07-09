9bc3aff36a750792c64324b5830220e78c4a3292
Amenable-C <duswo0624@naver.com>
2022-07-09 19:03:43 +0900

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+1. specification
+2. filter
+3. image(s3)
---
+++ src/main/java/com/example/study_3week/Study3weekApplication.java
@@ -0,0 +1,13 @@
+package com.example.study_3week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study3weekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Study3weekApplication.class, args);
+	}
+
+}
